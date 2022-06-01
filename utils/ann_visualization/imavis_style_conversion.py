38ff9702b94b69989b1af9d07f5ab3caf1f5f33b
simocampi <37673011+simocampi@users.noreply.github.com>
2022-06-01 18:10:28 +0200

    imavis style conversion

--- utils/ann_visualization/imavis_style_conversion.py
+++ utils/ann_visualization/imavis_style_conversion.py
@@ -1,62 +1,51 @@
 import json
+import os
 
 import imageio
 import numpy as np
 from utils.ann_visualization.joint import Joint
 from utils.ann_visualization.pose import Pose
 from path import Path
+import cv2
 
 from utils.ann_visualization.visualize import get_colors
 
 MAX_COLORS = 42
 
 
-def json_imavis_style_conversion(in_mp4_file_path, json_file_path, out_mp4_file_path, hide, bbox=False):
+def json_imavis_style_conversion(json_file_path):
     """
     Script that provides a visual representation of the annotations
     """
-    out_mp4_file_path = Path(out_mp4_file_path)
-    if not out_mp4_file_path.parent.exists() and out_mp4_file_path.parent != Path(''):
-        out_mp4_file_path.parent.makedirs()
-
-    reader = imageio.get_reader(in_mp4_file_path)
 
     with open(json_file_path, 'r') as json_file:
         data = json.load(json_file)
         data = np.array(data)
 
+    n_frames = int(data[-1][0])
+
     colors = get_colors(number_of_colors=MAX_COLORS, cmap_name='jet')
 
-    print(f'▸ visualizing annotations of \'{Path(in_mp4_file_path).abspath()}\'')
-    for frame_number, image in enumerate(reader):
+    new_data = []
 
-        # NOTE: frame #0 does NOT exists: first frame is #1
+    for frame_number in range(n_frames + 1):
+
+        # NOTE: frame #0 does NOT exist: first frame is #1
         frame_data = data[data[:, 0] == frame_number]  # type: np.ndarray
 
         for p_id in set(frame_data[:, 1]):
             pose = get_pose(frame_data=frame_data, person_id=p_id)
 
-            # if the "hide" flag is set, ignore the "invisible" poses
-            # (invisible pose = pose of which I do not see any joint)
-            if hide and pose.invisible:
-                continue
+            # get bbox of the ped:  ( x, y, width, height )
+            bbox = np.array(pose.bbox_2d_padded)
+            row = np.concatenate([[frame_number, p_id], bbox]).astype(int)
 
-            # select pose color base on its unique identifier
-            color = colors[int(p_id) % len(colors)]
+            new_data.append(list(row))
 
-            # draw pose on image
+        print(f'\r▸"Annotation seq_{j} progress: {100 * (frame_number / n_frames):6.2f}%', end='')
 
-            if bbox:
-                bbox = np.array(pose.bbox_2d_padded).astype(int)
-                image = cv2.rectangle(image, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), color, 2)
-            else:
-                image = pose.draw(image=image, color=color)
-
-        writer.append_data(np.vstack([image, image[-8:, :]]))
-        print(f'\r▸ progress: {100 * (frame_number / 1800):6.2f}%', end='')
-
-    writer.close()
-    print(f'\n▸ video with annotations: \'{out_mp4_file_path.abspath()}\'\n')
+    with open(os.path.join(seq_path, f"seq_{j}_imavis.json"), "w") as f:
+        json.dump(new_data, f)
 
 
 def get_pose(frame_data, person_id):
@@ -72,4 +61,6 @@ def get_pose(frame_data, person_id):
 
 
 if __name__ == "__main__":
-    pass
+    for j in range(18):
+        seq_path = f"C:\\Users\\simoc\\Desktop\\Synthetic Data IMAVIS\\seq_{j}"
+        json_imavis_style_conversion(os.path.join(seq_path, f"seq_{j}.json"))

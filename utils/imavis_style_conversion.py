833b782ba19845baa0a6f01db5cab856457b725a
Simone Campisi <37673011+simocampi@users.noreply.github.com>
2022-06-03 11:20:44 +0200

    starting Xml version

--- utils/imavis_style_conversion.py
+++ utils/imavis_style_conversion.py
@@ -25,6 +25,51 @@ LABEL_MAP = dict(
 )
 
 
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="\t")
+
+
+def create_xml_root(name_root):
+    return gfg.Element(name_root)
+
+
+def get_image_node(id_frame: int, frame_name: str, frame_width: int, frame_height: int):
+    image_node = gfg.Element("image")
+    image_node.set("id", str(id_frame))
+    image_node.set("name", frame_name)
+    image_node.set("width", str(frame_width))
+    image_node.set("height", str(frame_height))
+
+    return image_node
+
+
+def get_box_node(top_elem, label: int, xtl: int, ytl: int, xbr: int, ybr: int):
+    box_node = gfg.SubElement(top_elem, "box")
+    box_node.set("label", str(label))
+    box_node.set("xtl", str(xtl))
+    box_node.set("ytl", str(ytl))
+    box_node.set("xbr", str(xbr))
+    box_node.set("ybr", str(ybr))
+
+    return box_node
+
+
+def get_pose(frame_data, person_id):
+    # type: (np.ndarray, int) -> Pose
+    """
+    :param frame_data: data of the current frame
+    :param person_id: person identifier
+    :return: list of joints in the current frame with the required person ID
+    """
+    pose = [Joint(j) for j in frame_data[frame_data[:, 1] == person_id]]
+    pose.sort(key=(lambda j: j.type))
+    return Pose(pose)
+
+
 def json_imavis_style_conversion(json_file_path, seq_path):
     """
     Script that provides a visual representation of the annotations
@@ -60,52 +105,17 @@ def json_imavis_style_conversion(json_file_path, seq_path):
         json.dump(new_data, f)
 
 
-def prettify(elem):
-    """Return a pretty-printed XML string for the Element.
-    """
-    rough_string = ElementTree.tostring(elem, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent="\t")
-
-
-def create_xml_root(name_root):
-    return gfg.Element(name_root)
-
-
-def create_xml_annotations(root, id_frame, name, width, height):
-    image = gfg.Element("image")
-    image.set("id", id_frame)
-    image.set("name", name)
-    image.set("width", width)
-    image.set("height", height)
-
-    root.append(image)
-
-
-def get_pose(frame_data, person_id):
-    # type: (np.ndarray, int) -> Pose
-    """
-    :param frame_data: data of the current frame
-    :param person_id: person identifier
-    :return: list of joints in the current frame with the required person ID
-    """
-    pose = [Joint(j) for j in frame_data[frame_data[:, 1] == person_id]]
-    pose.sort(key=(lambda j: j.type))
-    return Pose(pose)
-
-
 if __name__ == "__main__":
     root = create_xml_root("annotations")
 
-    create_xml_annotations(root, str(0), "prova", str(1920), str(1080))
+    img = get_image_node(0, "prova1", 1920, 1080)
+    img.append(get_box_node(img, 0, 12, 45, 300, 450))
+    img.append(get_box_node(img, 0, 12, 45, 300, 450))
+    img.append(get_box_node(img, 0, 12, 45, 300, 450))
+
+    root.append(img)
 
     xml_str = prettify(root)
 
     with open("prova.xml", "w") as f:
         f.write(xml_str)
-
-    '''
-    for j in range(18):
-        seq_path = f"C:\\Users\\simoc\\Desktop\\Synthetic Data IMAVIS\\seq_{j}"
-        json_imavis_style_conversion(os.path.join(seq_path, f"seq_{j}.json"))
-    '''
